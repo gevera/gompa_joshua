8ceef4344a40902a955fbe5d5f3309b38e00ae2c
gevera <skryonline@gmail.com>
2021-12-01 21:09:05 +0200

    Added link to SKRYOnline in footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,11 +1,15 @@
 <div>
-	Gompa Apartments LLC - {new Date().getFullYear()}
+	<div class="borderb">Gompa Apartments LLC - {new Date().getFullYear()}</div>
+	<div>Made with compassion by <a href="https://skryonline.com" target="_blank">SKRY | Online</a></div>
 </div>
 
 <style>
 	div {
 		text-align: center;
 		margin: 2rem 0;
-		padding-top: 2rem;
+		padding: 2rem;
+	}
+	.borderb {
+		border-bottom: 2px solid #dadada;
 	}
 </style>
